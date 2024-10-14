1e0ed98e58a6e32df4460d9df2ddb428229bad85
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-10-14 10:43:06 +0545

    Create greeting_by_time.py

---
+++ greeting_by_time.py
@@ -0,0 +1,14 @@
+import time
+
+hour = int(time.strftime('%H'))
+print('Now Timing :', hour)
+name = input('Enter Your Name : ').title()
+
+if 0 <= hour < 12:
+    print('Good Morning', name)
+elif 12 <= hour <= 16:
+    print('Good Afternoon', name)
+elif 17 <= hour <= 20:
+    print('Good Evening', name)
+else:
+    print("Good Night", name)
