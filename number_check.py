f28e0a0fd9ee709fd11bd2405ddbc48c61d54cad
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-10-15 13:35:37 +0545

    Create number_check.py

---
+++ number_check.py
@@ -0,0 +1,15 @@
+a = int(input("Enter the num : "))
+if a < 0:
+    print("It is Negative!")
+elif a > 0:
+    print("It is Positive!")
+    if a <= 10:
+        print("It is Between 0-10!")
+    elif a <= 100:
+        print("It is Between 11-100!")
+    elif a <= 500:
+        print("It is Between 101-500!")
+    else:
+        print("It is Out of 500!")
+else:
+    print("It is Zero!")
