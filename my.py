a301572d68ab643ebf21c2ca061e5570591d1dee
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-10-15 13:46:21 +0545

    Add files via upload

---
+++ my.py
@@ -0,0 +1,17 @@
+                        # comment , Escape char , Parameter 
+                            
+print("hello world")
+print(3)
+print("Namste Anushka \nhow are you ") #this is used for single line comment (#)
+
+print(19+37)            #this is used for Arithmetic operation
+print(97*23)           
+                        #for single line comment to all lines we use (control + /)
+                        #for multiline comment we use """ or ''' 
+
+print("Hello my name is \'Anushka\' \nI am a good coder in \"Python\"") #It is Escape Sequance char "\n"  or  \'  or  \" 
+
+print("hyy" , 6 , 9 , sep = '-', end = " null ")
+print("MISTY")
+print('KAJU')  # we can use "" or '' in python code
+                                                                     
