781cfeff85c6ee4fcd306a084fc91328c7f589b2
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-10-15 13:37:22 +0545

    Add files via upload

---
+++ Casting.py
@@ -0,0 +1,31 @@
+    # Typecasting in Python
+#Basic type casting
+a = '8'
+b = '6'
+print(int(a)+int(b))
+
+
+#Explicit typecasing
+string = "1"
+number = 7
+string = int(string)
+sum = string + number
+print("The sum is : ",sum)
+
+
+
+#Implicit typecasing
+a = 1 
+b = 1.2
+print(a+b)
+
+c = 1 & 2
+d = 8
+print(c+d)
+
+e = 1 
+print(type(e))
+
+
+                      
+
