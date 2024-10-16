dca248fe99174b678c556b13b1aca147c453708a
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-10-16 23:28:57 +0545

    Add files via upload

---
+++ Datatype.py
@@ -0,0 +1,48 @@
+# Numaric Data type
+# INT 
+a = 32
+print(type(a))
+print(a)
+b = 2837494982048028
+print(b)
+print(a.__sizeof__())
+print(b.__sizeof__())
+
+# Float
+x = 1.3
+print(type(x))
+print(x)
+y = 1834783423.92348793
+print(y)
+a = 12.59E2
+print(a)
+z = 12.59e-2
+print(z)
+t = 12.59 * 100
+print(t)
+
+# Bool
+a =  True
+print(a)
+print(type(a))
+print(int(a))
+b = False
+print(b)
+print(int(b))
+print(int(True))
+print(int(False))
+
+# Complex  a + bj 
+s = 3+9j
+print(s)
+t = 2.4+3.21j
+print(t)
+print(complex(2.5,23))
+print(complex(63))
+print(complex(3.9j))
+# k = 3+9i
+print(s.imag)
+print(s.real)
+c = 3.9+6j
+print(c.imag)
+print(c.real)
