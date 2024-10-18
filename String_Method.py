a6d4edd5aa3bd59d2e0137792830113c8dff53ba
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-10-18 20:01:47 +0545

    Update String_Method.py

--- String_Method.py
+++ String_Method.py
@@ -82,3 +82,72 @@ print(s1.lstrip(".")) #it will remove only starting dots because sfter this sapc
 print(s1.lstrip('. +')) #it can remove multiple char
 print(s1.strip('. +k'))
 
+# capitalize() METHOD (it capitalize only first latter of string)
+string2 = "tu jhooti mai makkar"
+c = string2.capitalize()
+print(c)
+
+# title() METHOD (it capitalize first latter of each word in string)
+c = string2.title()
+print(c)
+
+# lower() METHOD (it makes lower to each word in str)
+c = string2.lower()
+print(c)
+
+# upper() METHOD (it makes each letter capital)
+c = string2.upper()
+print(c)
+
+# swapcase() METHOD (used to swap each upper to lower vica versa)
+swap = 'HEllo , SHivanANSH'
+c = swap.swapcase()
+print(c)
+
+# casefold() METHOD (it do work same as lower but minor diff is-)
+c = swap.casefold()
+print(c)
+
+# MINOR DIFFERENCE 
+a1 = 'heLLO'
+a2 = 'HELLO'
+print(a1 == a2)
+
+
+T = True
+F = False
+if a1.lower() == a2.lower():
+    print(T)
+else:
+    print(F)
+
+b1 = 'Bu\u00DF'
+print(b1)  # o/p Buß
+
+b2 = 'Buss'
+print(b1 == b2)
+print(b1.lower() == b2.lower()) # False
+print(b1.casefold() == b2.casefold())  #True
+
+t = '\u01C6'
+print(t)
+print(t.upper())
+print(t.title())
+
+# isupper() METHOD 
+print(string.isupper())
+print(string2.isupper())
+print(swap.isupper())
+b = 'HELLO'
+print(b.isupper())
+
+# islower() METHOD
+print(string.islower())
+print(string2.islower())
+print(swap.islower())
+
+# istitle() METHOD
+print(string.istitle())
+print(string2.istitle())
+print(swap.istitle())
+print(b2.istitle())
