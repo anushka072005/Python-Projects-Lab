5476d9ae1ed198e5829f12ccabe71849ede390d4
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-10-28 22:50:08 +0545

    Create product_price.py

---
+++ product_price.py
@@ -0,0 +1,12 @@
+ # 2) DISPLAY DATA IN GIVEN FORMAT (25 LETTERS)
+        # Product Name........Price
+        # Kathi role............100
+
+Product_name = input("Enter Product name : ")
+Price = input('Enter Price : ')
+
+total_len = len(Product_name) + len(Price)
+left_space = 25 - total_len
+dots = '.' * left_space
+
+print(f'{Product_name}{dots}{Price}')
