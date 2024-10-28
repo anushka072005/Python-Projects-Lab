7a281fa36979ffc4e18fe415de1e93cedd686f53
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-10-28 22:52:40 +0545

    Create password_check.py

---
+++ password_check.py
@@ -0,0 +1,12 @@
+# 3) CHECK IF THE PASSWORD AND CONFIRM PASSWORD ARE SAME
+password = input('Enter password : ')
+confirm_password = input('Enter confirm password : ')
+
+if password == confirm_password:
+    print('Yes, they are matching')
+
+else:
+    if password.casefold() == confirm_password.casefold():
+        print('Please check for the cases and try again')
+    else:
+        print('No they are not matching try again')
