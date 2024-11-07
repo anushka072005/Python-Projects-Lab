741b84cfd0e13f7f20a008dda88b6459c00a4295
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2024-11-07 23:16:41 +0545

    Create make_palindrome.py

---
+++ make_palindrome.py
@@ -0,0 +1,13 @@
+
+# 6) CONVERT STRING TO PALINDROME.
+
+string = input("enter string : ")
+
+reverseString = string[::-1]
+
+if string == reverseString:
+    print("palindrom")
+
+else:
+    convert = string + reverseString
+    print("converted palindrom : " , convert) 
