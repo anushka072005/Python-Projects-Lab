548b633d4c021a4dab336c2864ac6345a534dae2
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-03-19 14:39:45 +0545

    Update Domain_name_email.py

--- Domain_name_email.py
+++ Domain_name_email.py
@@ -1,5 +1,5 @@
 # 5) FIND USER-ID AND DOMAIN NAME FROM EMAIL. 
-email = input('Enter your email : ')   
+email = input('Enter your email : ')    
 check = email.index('@')
 print("User ID is :",email[0:check] )
 print("Domain name is :",email[check+1:len(email)] ) 
