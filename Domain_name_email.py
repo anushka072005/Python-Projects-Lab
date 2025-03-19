bca565f3393ff2e3a6d56d6d55ace25c50564b99
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-03-19 14:39:37 +0545

    Update Domain_name_email.py

--- Domain_name_email.py
+++ Domain_name_email.py
@@ -2,5 +2,5 @@
 email = input('Enter your email : ')   
 check = email.index('@')
 print("User ID is :",email[0:check] )
-print("Domain name is :",email[check+1:len(email)] )
+print("Domain name is :",email[check+1:len(email)] ) 
  
