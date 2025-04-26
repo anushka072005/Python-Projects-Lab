dec84c5a917f5be7e78e291ed057d016455007f7
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-04-26 15:13:30 +0545

    Update greeting_by_time.py

--- greeting_by_time.py
+++ greeting_by_time.py
@@ -8,7 +8,7 @@ if 0 <= hour < 12:
     print('Good Morning', name)
 elif 12 <= hour <= 16:
     print('Good Afternoon', name)
-elif 17 <= hour <= 20:
+elif 17 <= hour <= 20: 
     print('Good Evening', name)
 else:
     print("Good Night", name)
