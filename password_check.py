f66d4684d4e435bb9f90b7eeadd00410746d6383
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-09-17 20:10:59 +0000

    Update password_check.py

--- password_check.py
+++ password_check.py
@@ -4,7 +4,7 @@ confirm_password = input('Enter confirm password : ')
  
 if password == confirm_password: 
     print('Yes, they are matching') 
- 
+  
 else:
     if password.casefold() == confirm_password.casefold():
         print('Please check for the cases and try again')
