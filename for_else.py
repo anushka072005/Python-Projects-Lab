728d6ca23e53d9da31284c2b0411be5d3a68d2eb
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-10-10 14:55:31 +0000

    Add files via upload

---
+++ for_else.py
@@ -0,0 +1,21 @@
+# age = [1,5,3,67,764, 4,6]
+# for i in age:
+#     if (i > 100):
+#         break
+#     print(i)
+# else:
+#     print("Sorry no i")
+
+
+# for i in range(0):
+#     print("nothing",i)
+# else:
+#     print("No data found")
+
+
+for i in range(5):
+    print("iteration no {} in for loop".format(i+1))
+else:
+    print("else block in loop")
+    
+print("Out of loop")
