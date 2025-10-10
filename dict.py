41f785569b3c00e838c6209aa1b1dafdb58e3642
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-10-10 14:54:53 +0000

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,36 @@
+info = {
+    'name' : 'sunita',
+    'age' : 37,
+    'eligible' : True
+}
+# print(info)
+# print(info['name'])
+# print(info['class']).
+# print(info.get('name'))
+# print(info.get('class'))
+ 
+# for key in info.keys():
+#     print(f"The value corresponding to the key {key} is {info[key]}")
+
+# for key in info.values():
+#     pass
+#     print(key)
+
+# print(info.items())
+# for key,value in info.items():
+#     print(f"The value corresponding to the key {key} is {value}")
+
+
+# Dict Method
+
+ep1 = {1: 46, 2: 63, 3: 34, 4: 83}
+ep2 = {5: 23, 6: 29}
+
+# ep1.update(ep2)
+# ep1.clear()
+# del ep1
+# del ep1[4]
+# ep1.pop(4)
+# print(ep1[4])
+# ep1.popitem() 
+print()
