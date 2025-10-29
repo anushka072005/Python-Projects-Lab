ecf063a093c02c9ed334e76f8c0082d5bb499b7f
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-10-29 17:45:44 +0000

    Update product_price.py

--- product_price.py
+++ product_price.py
@@ -3,7 +3,7 @@
         # Kathi role............100    
 
 Product_name = input("Enter Product name : ")  
-Price = input('Enter Price : ')
+Price = input('Enter Price : ') 
  
 total_len = len(Product_name) + len(Price)
 left_space = 25 - total_len
