636b333ce80c21de1da7b7c55b919d5ed9a444a8
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2025-11-05 10:42:30 +0000

    Add files via upload

---
+++ IsEqual.py
@@ -0,0 +1,22 @@
+# list
+# a = [1,2,3]
+# b = [1,2,3]
+
+# constant
+# a = 3
+# b = 3
+
+# a = "hello"
+# b = "hello"
+
+# a = (1,2)
+# b = (1,2)
+
+a = None
+b = None
+ 
+# print(a is b)
+# print(a == b)
+
+print(a is b)
+print(a is None)
