137eaaa173fc8124c4475143fd7fbe02aafeb698
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2026-01-06 10:24:50 +0000

    Add files via upload

---
+++ mutable_immutable.py
@@ -0,0 +1,15 @@
+# Mutable and Immutable objects
+
+x = 10
+y = x
+print(x)
+print(y)
+
+# assigning new value to x 
+x = 15
+print(x)
+
+# see value of Y did'nt changed
+print(y)
+
+# python has inbuild garbege collector.
