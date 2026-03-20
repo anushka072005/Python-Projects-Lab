1d02e795f972b80c3b7343e89808a0ed45ee01b5
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2026-03-20 16:10:15 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 a = int(input("Enter your first digit: "))
 b = int(input("Enter your second digit: "))
-
+ 
 print("\n--- Results ---\n")
 print(f"➕ Addition      : {a} + {b} = {a + b}")
 print(f"➖ Subtraction   : {a} - {b} = {a - b}")
