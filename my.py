336ffa5bfc19857fd937be761f0226be804274af
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2026-04-09 01:05:16 +0530

    Update my.py

--- my.py
+++ my.py
@@ -2,7 +2,7 @@
                             
 print("hello world")
 print(3)
-print("Namste Anushka \nhow are you ") #this is used for single line comment (#)
+print("Namste Anushka \nhow are you ") #this is used for single line comment (#) 
 
 print(19+37)            #this is used for Arithmetic operation
 print(97*23)           
