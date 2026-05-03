f4e4296f352cdf4247e4a34e85037b4f9e244456
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2026-05-04 02:28:59 +0530

    Update Nested_loop.py

--- Nested_loop.py
+++ Nested_loop.py
@@ -8,7 +8,7 @@
 #     for j in range(0,5): 
 #         print(i, j, end=" ")
 
-# for i in range(0,5):
+# for i in range(0,5): 
 #     for j in range(0,5): 
 #         print(i, j, end= " ")  # end= using for space  
 #     print(" ")  # for next line 
