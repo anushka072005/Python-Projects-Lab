6ddd04363bc48b3c85c5ec0220874c6b1d2221eb
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2026-05-28 14:24:44 +0530

    Add files via upload

---
+++ Restorant_menu.py
@@ -0,0 +1,20 @@
+def Restaurant():
+    items = []
+    for i in range(4):
+        item = input("Enter item name : ")
+        price = input("Enter item price : ")
+
+        totalLen = len(item) + len(price)
+        desh = 20 - totalLen
+
+        output = f"{item}{'-' * desh}${price}"
+        items.append(output)
+
+    print("\n----- MENU -----")
+
+    for i in items:
+        print(i)    
+
+Restaurant()
+
+
