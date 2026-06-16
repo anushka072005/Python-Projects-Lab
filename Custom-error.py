d1b9c546e6eda0bffdbfe225b8ac7f7ebc5cd3a8
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2026-06-17 01:52:48 +0530

    Update Custom-error.py

--- Custom-error.py
+++ Custom-error.py
@@ -1,6 +1,6 @@
 # a = int(input("Enter any Value between 5 and 9 : "))
 
-# if(a<5 or a>9): 
+# if(a<5 or a>9): m
 #     raise ValueError("Value Should be between 5 and 9")
 # else:
 #     print(a)    
