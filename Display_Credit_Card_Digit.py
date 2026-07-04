42a90371790ff9e1a92cb6a8228cadc67dd1c517
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2026-07-04 22:23:34 +0530

    Update Display_Credit_Card_Digit.py

--- Display_Credit_Card_Digit.py
+++ Display_Credit_Card_Digit.py
@@ -5,7 +5,7 @@ print('Please Credit Card Number In This Format i.e. 1111 2222 3333 4444')
 user_input = input('Enter credit card number : ')     
  
 last_4_digit = user_input[15:] 
-
+ 
 four = '*' * 4 + ' '
 
 display = four * 3 + last_4_digit
