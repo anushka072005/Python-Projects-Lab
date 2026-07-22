366cfb6972725d1346fac3657466627d25fa90a5
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2026-07-22 17:32:58 +0530

    Update Nested_loop.py

--- Nested_loop.py
+++ Nested_loop.py
@@ -2,7 +2,7 @@
      
 # for i in range(0,5):        
 #     for j in range(0,5):          
-#         print(i,j)    
+#         print(i,j)     
    
 # for i in range(0,5):  
 #     for j in range(0,5): 
