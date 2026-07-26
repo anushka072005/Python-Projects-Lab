1033ea0e3254b288e837c67ce808929d9f3d40ca
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2026-07-26 23:28:02 +0530

    Update ExceptionHandling.py

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -5,7 +5,7 @@
 #     for i in range(1,11):  
 #         print(f"{int(a)} X {i} = {int(a)*i}")
 # except Exception as e:
-#     print(e)
+#     print(e) 
 
 # print("Some imp lines of code")        
 # print("End of program")
