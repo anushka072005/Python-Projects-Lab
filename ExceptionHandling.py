c74b634688ec61bb8a5bf8df775b4070ef9b7400
Anushka Patkar <144523727+anushka072005@users.noreply.github.com>
2026-07-26 23:05:48 +0530

    Update ExceptionHandling.py

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -2,7 +2,7 @@
 #     a = int(input("Enter the number : ")) 
 #     print(f"Multiplication table of {a} is ")
   
-#     for i in range(1,11): 
+#     for i in range(1,11):  
 #         print(f"{int(a)} X {i} = {int(a)*i}")
 # except Exception as e:
 #     print(e)
